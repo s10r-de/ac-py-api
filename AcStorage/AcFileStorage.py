17d00358d06a2153e814f4178bd76731f67908c7
Carsten <cschaba@s10r.de>
2024-08-15 17:15:07 +0200

    Cleanup
    Task#11

--- AcStorage/AcFileStorage.py
+++ AcStorage/AcFileStorage.py
@@ -2,8 +2,10 @@ import json
 import os
 import shutil
 
+from ActiveCollabAPI.AcProject import AcProject
 from ActiveCollabAPI.AcTask import AcTask
 
+DEFAULT_MODE_DIRS = 0o700
 
 class AcFileStorage(object):
 
@@ -19,6 +21,8 @@ class AcFileStorage(object):
         if not os.path.exists(self.get_account_path()):
             os.makedirs(self.get_account_path(), 0o700)
             os.makedirs(self.get_tasks_path(), 0o700)
+            os.makedirs(self.get_account_path(), DEFAULT_MODE_DIRS)
+            os.makedirs(self.get_projects_path(), DEFAULT_MODE_DIRS)
 
     def get_account_path(self) -> str:
         return os.path.join(self.root_path, "account-%08d" % self.account_id)
@@ -39,3 +43,20 @@ class AcFileStorage(object):
 
     def get_task_full_filename(self, task_filename: str) -> str:
         return os.path.join(self.get_tasks_path(), task_filename)
+
+    def get_projects_path(self) -> str:
+        return os.path.join(self.get_account_path(), "projects")
+
+    def get_project_filename(self, project: AcProject) -> str:
+        return "project-%08d.json" % project.id
+
+    def get_project_full_filename(self, project_filename: str) -> str:
+        return os.path.join(self.get_projects_path(), project_filename)
+
+    def save_project(self, project: AcProject) -> str:
+        assert (project.class_ == "Project")
+        project_filename = self.get_project_filename(project)
+        project_full_filename = self.get_project_full_filename(project_filename)
+        with open(project_full_filename, "w") as f:
+            json.dump(project.to_dict(), f, sort_keys=True, indent=2)
+        return project_full_filename
