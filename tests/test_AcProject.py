29c2301d95da49dc54dfd8f361667e357cfc33a1
Carsten <cschaba@s10r.de>
2024-08-16 09:37:47 +0200

    Update dump project data and related active tasks
    Task#11

--- tests/test_AcProject.py
+++ tests/test_AcProject.py
@@ -1,73 +1,30 @@
 import json
 from unittest import TestCase
 
-from ActiveCollabAPI.AcProject import AcProject
+from ActiveCollabAPI.AcProject import AcProject, project_from_json
 
 
 class TestAcProject(TestCase):
 
-    def _test_project(self, project_id):
-        return AcProject(
-            based_on_id=0,
-            based_on_type=None,
-            body="",
-            body_formatted="",
-            budget=0.0,
-            budget_type="",
-            budgeting_interval="",
-            category_id=0,
-            class_="Project",
-            company_id=0,
-            completed_by_id=0,
-            completed_on=0,
-            count_discussions=0,
-            count_files=0,
-            count_notes=0,
-            count_tasks=0,
-            created_by_email="",
-            created_by_id=0,
-            created_by_name="",
-            created_on=0,
-            currency_id=0,
-            email="",
-            file_size=0,
-            id=project_id,
-            is_billable=False,
-            is_client_reporting_enabled=False,
-            is_completed=False,
-            is_estimate_visible_to_subcontractors=False,
-            is_sample=False,
-            is_tracking_enabled=False,
-            is_trashed=False,
-            label_id=0,
-            last_activity_on=0,
-            leader_id=0,
-            members=[1, 2],
-            members_can_change_billable=False,
-            name="",
-            project_number=0,
-            show_task_estimates_to_clients=False,
-            task_estimates_enabled=False,
-            trashed_by_id=0,
-            trashed_on=False,
-            updated_by_id=0,
-            updated_on=0,
-            url_path=""
-        )
+    def _generate_test_project(self, project_id: int) -> AcProject:
+        with open('../example-data/example-project-611.json', 'r') as fh:
+            project = project_from_json(json.load(fh))
+        project.id = project_id
+        return project
 
     def test_constructor(self):
-        project_id = 554433
-        project = self._test_project(project_id)
+        project_id = 611
+        project = self._generate_test_project(project_id)
         self.assertEqual(project.id, project_id)
 
     def test_to_dict(self):
-        project_id = 554433
-        project = self._test_project(project_id)
+        project_id = 611
+        project = self._generate_test_project(project_id)
         project_dict = project.to_dict()
         self.assertEqual(project_dict["id"], project_id)
 
     def test_to_json(self):
-        project_id = 554433
-        project = self._test_project(project_id)
+        project_id = 611
+        project = self._generate_test_project(project_id)
         project_json = project.to_json()
         self.assertEqual(json.loads(project_json)["id"], project_id)
--- tests/test_AcTask.py
+++ tests/test_AcTask.py
@@ -1,73 +1,30 @@
 import json
 from unittest import TestCase
 
-from ActiveCollabAPI.AcTask import AcTask
-from ActiveCollabAPI.AcTaskDependencies import AcTaskDependencies
+from ActiveCollabAPI.AcTask import AcTask, task_from_json
 
 
 class TestAcTask(TestCase):
-    def _test_task(self, task_id: int):
-        return AcTask(
-            assignee_id=0,
-            attachments=[],
-            body="test body",
-            body_formatted="test body formatted",
-            body_mode="",
-            class_="Task",
-            comments_count=0,
-            completed_by_id=0,
-            completed_on=0,
-            completed_subtasks=0,
-            created_by_email="",
-            created_by_id=0,
-            created_by_name="",
-            created_from_recurring_task_id=0,
-            created_on=0,
-            delegated_by_id=0,
-            due_on=0,
-            estimate=0,
-            fake_assignee_email="",
-            fake_assignee_name="",
-            id=task_id,
-            is_billable=False,
-            is_completed=False,
-            is_hidden_from_clients=False,
-            is_important=False,
-            is_trashed=False,
-            job_type_id=0,
-            labels=[],
-            name="Task Name",
-            open_dependencies=AcTaskDependencies(
-                parents_count=0,
-                children_count=0
-            ),
-            open_subtasks=0,
-            position=0,
-            project_id=0,
-            start_on=0,
-            task_list_id=0,
-            task_number=0,
-            total_subtasks=0,
-            trashed_by_id=0,
-            trashed_on=0,
-            updated_by_id=0,
-            updated_on=0,
-            url_path=""
-        )
+
+    def _generate_test_task(self, task_id: int) -> AcTask:
+        with open('../example-data/example-task-17614.json', 'r') as fh:
+            task = task_from_json(json.load(fh))
+        task.id = task_id
+        return task
 
     def test_actask_constructor(self):
-        task_id = 123456
-        task = self._test_task(task_id)
+        task_id = 17614
+        task = self._generate_test_task(task_id)
         self.assertEqual(task_id, task.id)
 
     def test_to_dict(self):
-        task_id = 123456
-        task = self._test_task(task_id)
+        task_id = 17614
+        task = self._generate_test_task(task_id)
         task_dict = task.to_dict()
         self.assertEqual(task_dict["id"], task_id)
 
     def test_to_json(self):
-        task_id = 123456
-        task = self._test_task(task_id)
+        task_id = 17614
+        task = self._generate_test_task(task_id)
         task_json = task.to_json()
         self.assertEqual(json.loads(task_json)["id"], task_id)
