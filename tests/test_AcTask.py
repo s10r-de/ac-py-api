45900b6d8474a653dc1df319009d7244d40c7ec5
Carsten <cschaba@s10r.de>
2024-09-12 14:27:08 +0200

    Fix add Tests for Task Dependencies
    Task#15

--- tests/test_AcTask.py
+++ tests/test_AcTask.py
@@ -1,6 +1,7 @@
 import json
 from unittest import TestCase
 
+from AcTaskDependencies import AcTaskDependencies
 from ActiveCollabAPI.AcTask import AcTask, task_from_json
 
 
@@ -28,3 +29,24 @@ class TestAcTask(TestCase):
         task = self._generate_test_task(task_id)
         task_json = task.to_json()
         self.assertEqual(json.loads(task_json)["id"], task_id)
+
+    def test_to_dict_with_dependencies(self):
+        task_id = 17614
+        task = self._generate_test_task(task_id)
+        task.open_dependencies = AcTaskDependencies(parents_count=2, children_count=3)
+        task.attachments = None
+        task_dict = task.to_dict()
+        self.assertEqual(task_dict["id"], task_id)
+        self.assertEqual(task_dict["open_dependencies"]["parents_count"], 2)
+        self.assertEqual(task_dict["open_dependencies"]["children_count"], 3)
+
+    def test_to_json_with_dependencies(self):
+        task_id = 17614
+        task = self._generate_test_task(task_id)
+        task.open_dependencies = AcTaskDependencies(parents_count=2, children_count=3)
+        task.attachments = None
+        task_json = task.to_json()
+        parsed_json = json.loads(task_json)
+        self.assertEqual(parsed_json["id"], task_id)
+        self.assertEqual(parsed_json["open_dependencies"]["parents_count"], 2)
+        self.assertEqual(parsed_json["open_dependencies"]["children_count"], 3)
