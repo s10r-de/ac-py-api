e455685b6cf5a17cb54dcd112339062b6c3a6960
Carsten <cschaba@s10r.de>
2024-10-14 12:41:55 +0200

    Add missing test for Attachment
    Task#41

--- tests/test_AcFileAttachment.py
+++ tests/test_AcFileAttachment.py
@@ -20,37 +20,6 @@ class TestAcFileStorageAttachment(TestCase):
         attachment["id"] = attachment_id
         return attachment
 
-    def test_get_path(self):
-        account_id = ACCOUNT_ID
-        storage = AcFileStorageAttachment(DATA_DIR, account_id)
-        self.assertGreater(len(storage.get_path()), 1)
-
-    def test_reset(self):
-        account_id = ACCOUNT_ID
-        storage = AcFileStorageAttachment(DATA_DIR, account_id)
-        storage.reset()
-        self.assertFalse(os.path.isdir(storage.get_path()))
-
-    def test_ensure_dirs(self):
-        account_id = ACCOUNT_ID
-        storage = AcFileStorageAttachment(DATA_DIR, account_id)
-        storage.ensure_dirs()
-        self.assertTrue(os.path.isdir(storage.get_path()))
-
-    def test_get_filename(self):
-        account_id = ACCOUNT_ID
-        storage = AcFileStorageAttachment(DATA_DIR, account_id)
-        task = attachment_from_json(self._generate_test_attachment(55))
-        filename = storage.get_filename(task)
-        self.assertGreater(len(filename), 0)
-
-    def test_get_full_filename(self):
-        account_id = ACCOUNT_ID
-        storage = AcFileStorageAttachment(DATA_DIR, account_id)
-        task = attachment_from_json(self._generate_test_attachment(56))
-        filename = storage.get_filename(task)
-        full_filename = storage.get_full_filename(filename)
-        self.assertGreater(len(full_filename), 0)
 
     def test_save(self):
         account_id = ACCOUNT_ID
@@ -63,10 +32,15 @@ class TestAcFileStorageAttachment(TestCase):
         # FIXME test attachment
         self.assertGreater(len(full_filename), 0)
         self.assertTrue(os.path.isfile(full_filename))
-        # test catch the wrong class
-        task2 = attachment_from_json(self._generate_test_attachment(58))
-        task2.class_ = "dummy"
+
+    def test_save_wrong_class(self):
+        account_id = ACCOUNT_ID
+        storage = AcFileStorageAttachment(DATA_DIR, account_id)
+        storage.reset()
+        storage.ensure_dirs()
+        task = attachment_from_json(self._generate_test_attachment(58))
+        task.class_ = "dummy"
+        tmp_filename = mkstemp()[1]
         with self.assertRaises(AssertionError) as cm:
-            storage.save(task2, tmp_filename)
-        # FIXME assert tmp file not existing
+            storage.save(task, tmp_filename)
         self.assertEqual(AC_ERROR_WRONG_CLASS, cm.exception.args[0])
