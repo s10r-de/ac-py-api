220e754ed96cd956fbed5dc23517eb4251601031
Carsten <cschaba@s10r.de>
2024-10-21 14:14:59 +0200

    Add create projects from dump
    Task#45

--- AcStorage/AcFileStorageProject.py
+++ AcStorage/AcFileStorageProject.py
@@ -1,5 +1,5 @@
 from AcFileStorageBaseClass import AcFileStorageBaseClass
-from AcProject import AcProject
+from AcProject import AcProject, project_from_json
 from ActiveCollabAPI import AC_CLASS_PROJECT, AC_ERROR_WRONG_CLASS
 
 
@@ -16,3 +16,7 @@ class AcFileStorageProject(AcFileStorageBaseClass):
     def save(self, project: AcProject) -> str:
         assert project.class_ == AC_CLASS_PROJECT, AC_ERROR_WRONG_CLASS
         return super().save_with_id(project, project.id)
+
+    def load(self, project_id: int) -> AcProject:
+        project = self.load_by_id(project_id)
+        return project_from_json(project)
