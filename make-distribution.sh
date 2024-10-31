92729ba737390e13591cabf0fbb15aa32a42512b
Carsten <cschaba@s10r.de>
2024-10-31 13:54:27 +0100

    Add script to make a distribution file
    Task#16 - [task-link]

---
+++ make-distribution.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=$(date -I)
+PACKAGE="backup-active-collab_$VERSION"
+
+gtar cvzf "../$PACKAGE.tar.gz" --transform "s+^+$PACKAGE/+"  \
+	--exclude ".DS_Store" \
+	--exclude "._*" \
+	--exclude ".git/" \
+	--exclude ".idea" \
+	--exclude ".venv" \
+	--exclude "*.pyc" \
+	--exclude "__pycache__" \
+	--exclude "data*" \
+	--exclude "*.ini" \
+	*
+
