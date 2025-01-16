3d1d5ca95a002be1b1cfe8662b08ee08c07f7e5c
Carsten <cschaba@s10r.de>
2025-01-16 16:50:23 +0100

    Add simple caching to tasks
    Task#111

--- active_collab_storage/task.py
+++ active_collab_storage/task.py
@@ -11,20 +11,24 @@ class AcFileStorageTask(AcFileStorageBaseClass):
         super().__init__(root_path, account_id)
         self.filename_prefix = "task"
         self.dir_name = "tasks"
+        self.data: Iterator[AcTask] | None = None
 
     def setup(self):
         pass
 
     def save(self, task: AcTask) -> str:
         assert task.class_ == AC_CLASS_TASK, AC_ERROR_WRONG_CLASS
+        self.data = None
         return super().save_with_id(task, task.id)
 
     def load(self, task_id: int) -> AcTask:
         task = self.load_by_id(task_id)
         return task_from_json(task)
 
-    def get_all(self):
-        return map(task_from_json, super().get_all())
+    def get_all(self) -> Iterator[AcTask]:
+        if self.data is None:
+            self.data = list(map(task_from_json, super().get_all()))
+        return self.data
 
     def find_by_project(self, project_id: int) -> Iterator[AcTask]:
         return filter(lambda task: task.project_id == project_id, self.get_all())
