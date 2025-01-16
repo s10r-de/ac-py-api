0f29067746ad5c3eefc4582069e508b0e25db5e7
Carsten <cschaba@s10r.de>
2025-01-16 16:51:01 +0100

    Add selector and simple caching to subtasks
    Task#111

--- active_collab_storage/subtask.py
+++ active_collab_storage/subtask.py
@@ -1,6 +1,7 @@
+from typing import Iterator
+
 from active_collab_api import AC_CLASS_SUBTASK, AC_ERROR_WRONG_CLASS
 from active_collab_api.ac_subtask import AcSubtask, subtask_from_json
-
 from .base import AcFileStorageBaseClass
 
 
@@ -9,14 +10,27 @@ class AcFileStorageSubtask(AcFileStorageBaseClass):
         super().__init__(root_path, account_id)
         self.filename_prefix = "subtask"
         self.dir_name = "subtasks"
+        self.data = None
 
     def setup(self):
         pass
 
     def save(self, subtask: AcSubtask) -> str:
         assert subtask.class_ == AC_CLASS_SUBTASK, AC_ERROR_WRONG_CLASS
+        self.data = None
         return super().save_with_id(subtask, subtask.id)
 
     def load(self, subtask_id: int) -> AcSubtask:
         subtask = self.load_by_id(subtask_id)
         return subtask_from_json(subtask)
+
+    def get_all(self):
+        if self.data is None:
+            self.data = list(map(subtask_from_json, super().get_all()))
+        return self.data
+
+    def find_by_task(self, task_id: int) -> Iterator[AcSubtask]:
+        return filter(lambda t: t.task_id == task_id, self.get_all())
+
+    def sort_by_position(self, subtasks: Iterator[AcSubtask]) -> Iterator[AcSubtask]:
+        return sorted(subtasks, key=lambda t: t.position)
